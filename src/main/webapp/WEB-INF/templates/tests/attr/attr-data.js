80a4f279c3090ae2f2e95a252a63c87969d502e0
jjbenson <jjbenson@localhost>
2014-07-16 21:15:22 +0100

    New processAttr implementation using partial parsing. Test and test data support.

--- src/main/webapp/WEB-INF/templates/tests/attr/attr-data.js
+++ src/main/webapp/WEB-INF/templates/tests/attr/attr-data.js
@@ -7,13 +7,17 @@ var	thMappings = [
    		["/subscribe",		"/subscribe"]
    	];
 
+var affiliate = {};
+affiliate.identificationCode1 = "1234";
+affiliate.identificationCode2 = "5678";
+
 var	thVars = [
    		    ["product",		"#{ 'name': 'Lettuce', 'prices': { 'euros': '9.00', 'dollars': '12.00' } }"],
    		    ["identifier",		32],
-   		    ["sel",		true]
+   		    ["sel",		true],
+   		    ["affiliate",    affiliate]
    		];
 
 var	thMessages = [
    	   		["subscribe.submit",		"Subscribe me please!"]
    		];
-
--- src/test/java/org/thymoljs/thymol/test/selenium/cases/AttrCases.java
+++ src/test/java/org/thymoljs/thymol/test/selenium/cases/AttrCases.java
@@ -1,8 +1,10 @@
 package org.thymoljs.thymol.test.selenium.cases;
 
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertEquals;
-import org.thymoljs.thymol.test.selenium.SeleniumCases;
 
+import org.thymoljs.thymol.test.selenium.ResultMode;
+import org.thymoljs.thymol.test.selenium.SeleniumCases;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -19,6 +21,18 @@ public class AttrCases extends SeleniumCases {
 			"</form>" +
 			"\n\n\n";
 	
+	
+	
+	String attr01aResult =
+			"\n" +
+			"<form action=\"/subscribe\">\n" +
+			"  <fieldset>\n" +
+			"    <input type=\"text\" name=\"email\">\n" +
+			"    <input type=\"submit\" value=\"Subscribe me please!\">\n" +
+			"  </fieldset>\n" +
+			"</form>" +
+			"\n\n\n";
+	
 	String attr02Result = 					
 			"\n" +
 			"<span class=\"separate\">..</span>" +
@@ -28,8 +42,6 @@ public class AttrCases extends SeleniumCases {
 //			"\n" +
 //			"<span class=\"separate\" id=\"33\" title=\"Lettuce\">..</span>" +
 //			"\n\n\n";
- 	//<span class="separate" id="33" title="Lettuce">..</span>
-	//<span th:attr="class='separate',id=(${identifier} + 1),title=${product.name}">..</span>
 
 	String attr04Result = 					
 			"\n" +
@@ -44,26 +56,56 @@ public class AttrCases extends SeleniumCases {
 			"<option>..</option>\n" +
 			"<option>..</option>" +
 			"\n\n\n";
- 	
+	
+	String attr05ResultThymol = 					
+			"\n" +
+			"  <ul>\n" +
+			"    <li><a href=\"#1234\">Link v1</a></li>\n" +
+			"    <li><a data-target=\"#1234\" href=\"#\">Link v2</a></li>\n" +
+			"    <li><a data-target=\"#1234\" href=\"#\">Link v3</a></li>\n" +
+			"    <li><a data-target=\"#5678\" href=\"#\">Link v4</a></li>\n" +
+			"    <li><a data-target=\"#1234\" href=\"#\">Link v5</a></li>\n" +
+			"    <li><a data-target=\"#5678\" href=\"#\">Link v6</a></li>\n" +
+			"  </ul>\n" +
+			"\n";	
+	
+	String attr05ResultThymeleaf = 					
+			"\n" +
+			"  <ul>\n" +
+			"    <li><a href=\"#1234\">Link v1</a></li>\n" +
+			"    <li><a href=\"#\" data-target=\"#1234\">Link v2</a></li>\n" +
+			"    <li><a href=\"#\" data-target=\"#1234\">Link v3</a></li>\n" +
+			"    <li><a href=\"#\" data-target=\"#5678\">Link v4</a></li>\n" +
+			"    <li><a href=\"#\" data-target=\"#1234\">Link v5</a></li>\n" +
+			"    <li><a href=\"#\" data-target=\"#5678\">Link v6</a></li>\n" +
+			"  </ul>\n" +
+			"\n";	
+	
 	@Test
 	public void attr01() {
 		localise("tests/attr/");
-		String result = getResult( "attr01.html", false );
+		String result = getResult( "attr01.html", ResultMode.HTML );
 		result = result.replaceAll(";jsessionid=[^\"]*", "");
-		assertEquals( attr01Result, result );
+		result = clean(result);
+		if( result.equals( clean(attr01Result) ) ) {
+			assertEquals(clean(attr01Result),result);
+		}
+		else {
+			assertEquals(clean(attr01aResult),result);			
+		}
 	}
 
 	@Test
 	public void attr02() {
 		localise("tests/attr/");
-		String result = getResult( "attr02.html", false );
-		assertEquals( attr02Result, result );
+		String result = getResult( "attr02.html", ResultMode.HTML );
+		assertEquals( clean(attr02Result), clean(result) );
 	}
 	
 	@Test
 	public void attr03() {
 		localise("tests/attr/");
-		WebElement result = getResultBody( "attr03.html" );
+		WebElement result = getResultBody( "attr03.html", ResultMode.TEXT );
 		WebElement span = result.findElement( By.tagName( "span" ) );
 		String classAttr = span.getAttribute( "class" );
 		assertEquals( "separate", classAttr );
@@ -77,8 +119,20 @@ public class AttrCases extends SeleniumCases {
 	@Test
 	public void attr04() {
 		localise("tests/attr/");
-		String result = getResult( "attr04.html", false );
-		assertEquals( attr04Result, result );
+		String result = getResult( "attr04.html", ResultMode.HTML );
+		assertEquals( clean(attr04Result), clean(result) );
+	}
+	
+	@Test
+	public void attr05() {
+		localise("tests/attr/");
+		String result = getResult( "attr05.html", ResultMode.HTML );
+		if( expectThymolResult() ) {
+			assertEquals( clean(attr05ResultThymol), clean(result) );			
+		}
+		else {			
+			assertEquals( clean(attr05ResultThymeleaf), clean(result) );			
+		}
 	}
 
 }
