bc829e0886b411e1426f1562ef28f63ba3f933ed
jjbenson <jim.benson@live.co.uk>
2014-08-04 18:22:26 +0100

    Test data for new standard URL parser.

---
+++ src/main/webapp/WEB-INF/templates/tests/link/link-data.js
@@ -0,0 +1,18 @@
+var	thRoot="/";
+
+
+var	thDebug=true;
+var thShowNullOperands=false;
+var thProtocol="";
+
+var	thVars = [
+   	   	    ["base_url2",	"/base/url"],
+   	   	    ["user2",		"#{ 'name':'John Apricot', 'firstName':'John', 'lastName':'Apricot', 'nationality':'Antarctica','age':'(no age specified)'}"]
+     ];
+
+var	thMessages = [
+     	   	["salute",	"<b>Hello</b>, {0} ''The Great''!"],
+       	   	["name_is",		"The name is {0}"],
+       	   	["url.user.list",		"/user/list"]
+     ];
+
