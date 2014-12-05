695f127ff7de0c5ab277a31844189de31a0d71d8
jjbenson <jim.benson@live.co.uk>
2014-12-05 23:58:44 +0000

    Added Russian locale test examples. Added fixes for local time dependent tests. (Force use of UTC for JavaScript cases).

--- src/test/java/org/thymoljs/thymol/test/selenium/issues/IssuesCases.java
+++ src/test/java/org/thymoljs/thymol/test/selenium/issues/IssuesCases.java
@@ -2,6 +2,8 @@ package org.thymoljs.thymol.test.selenium.issues;
 
 import static junit.framework.Assert.assertEquals;
 
+import java.util.Locale;
+
 import org.junit.Test;
 import org.thymoljs.thymol.test.selenium.ResultMode;
 import org.thymoljs.thymol.test.selenium.SeleniumCases;
@@ -22,18 +24,16 @@ public class IssuesCases extends SeleniumCases {
 			"</table>\n" +
 			"\n";
 
-	String issue06Result = 
+	String issue06ResultThymol = 
+			"\n" +
+			"<span>Wed Oct 09 00:00:00 GMT 1940</span>\n" +
+			"\n";
+
+	String issue06ResultThymeleaf = 
 			"\n" +
 			"<span>Wed Oct 09 00:00:00 BST 1940</span>\n" +
 			"\n";
 
-	String issue06aResult = 
-			"\n" +
-			"<span>Wed Oct 09 00:00:00 BST 1940</span>\n" +
-			"\n";
-
-//	\n<h1>Product list</h1>\n<table>\n<tbody><tr>\n<th rowspan="1" colspan="1">NAME</th>\n<th rowspan="1" colspan="1">PRICE</th>\n</tr>\n<tr>\n<td rowspan="1" colspan="1">Potatoes</td>\n<td rowspan="1" colspan="1">�2.43</td>\n</tr>\n</tbody></table>\n\n\n	
-	
 	String issue08Result = 
 			"\n" +
 			"<h1>Product list</h1>\n" +
@@ -44,7 +44,23 @@ public class IssuesCases extends SeleniumCases {
 			"</tr>\n" +
 			"<tr>\n" +
 			"<td rowspan=\"1\" colspan=\"1\">Potatoes</td>\n" +
-			"<td rowspan=\"1\" colspan=\"1\">�2.43</td>\n" +
+			"<td rowspan=\"1\" colspan=\"1\">£2.43</td>\n" +
+			"</tr>\n" +
+			"</tbody></table>\n" +
+			"\n\n";
+
+	String issue08Result_ru_RU = 
+			"\n" +
+			"<h1>Product list</h1>\n" +
+			"<table>\n" +
+			"<tbody><tr>\n" +
+			"<th rowspan=\"1\" colspan=\"1\">NAME</th>\n" +
+			"<th rowspan=\"1\" colspan=\"1\">PRICE</th>\n" +
+			"</tr>\n" +
+			"<tr>\n" +
+//			"<td rowspan=\"1\" colspan=\"1\">\u041A\u0430\u0440\u0442\u043E\u0444\u0435\u043B\u044C</td>\n" +
+			"<td rowspan=\"1\" colspan=\"1\">Картофель</td>\n" +
+			"<td rowspan=\"1\" colspan=\"1\">2.43</td>\n" +
 			"</tr>\n" +
 			"</tbody></table>\n" +
 			"\n\n";
@@ -60,14 +76,24 @@ public class IssuesCases extends SeleniumCases {
 	public void issue06() {
 		localise("issues/");
 		String result = getResult( "issue06.html", ResultMode.HTML );
-		assertEquals( clean( issue06Result ), clean( result ) );
+		if( expectThymolResult() ) {
+			assertEquals( clean( issue06ResultThymol ), clean( result ) );
+		}
+		else {
+			assertEquals( clean( issue06ResultThymeleaf ), clean( result ) );
+		}
 	}
 
 	@Test
 	public void issue06a() {
 		localise("issues/");
 		String result = getResult( "issue06a.html", ResultMode.HTML );
-		assertEquals( clean( issue06aResult ), clean( result ) );
+		if( expectThymolResult() ) {
+			assertEquals( clean( issue06ResultThymol ), clean( result ) );
+		}
+		else {
+			assertEquals( clean( issue06ResultThymeleaf ), clean( result ) );
+		}
 	}
 
 	@Test
@@ -77,5 +103,11 @@ public class IssuesCases extends SeleniumCases {
 		assertEquals( clean( issue08Result ), clean( result ) );
 	}
 
+	@Test
+	public void issue08_ru_RU() {
+		localise("issues/", new Locale( "ru", "RU", "" ) );
+		String result = getResult("issue08_russian.html", ResultMode.HTML);
+		assertEquals(clean(issue08Result_ru_RU), clean(result));
+	}
 
 }
