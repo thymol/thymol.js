aaa2e1905249df3745c6bb8b64212bd9e882d032
jjbenson <jim.benson@live.co.uk>
2014-12-19 17:46:32 +0000

    Remove old comment lines.

--- src/main/webapp/WEB-INF/templates/v21/extrathyme/thymol/extrathyme.js
+++ src/main/webapp/WEB-INF/templates/v21/extrathyme/thymol/extrathyme.js
@@ -2,9 +2,6 @@
   This is a Thymol extension to emulate the behaviour of the "extrathyme" example dialect from http://www.thymeleaf.org/doc/html/Extending-Thymeleaf.html.
  */
 
-//thRoot = "${project.basedir}/dist/Webcontent/examples/templates/v21/extrathyme/webapp";
-//thMessagePath = thRoot + "/../resources";
-
 var	thProtocol = "";
 var	thRelativeRootPath = "Webcontent/examples/templates/v21/extrathyme/webapp";
 var     thMessagePath = "../resources";
