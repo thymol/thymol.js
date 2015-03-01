f10fc8d2dfd5d74f98293fdcf17364c59fdbe437
jjbenson <jim.benson@live.co.uk>
2015-03-01 20:03:40 +0000

    Tidied up banners.

--- dist/thymol-full.js
+++ dist/thymol-full.js
@@ -1,7 +1,7 @@
 /*-------------------- Thymol - the flavour of Thymeleaf --------------------*
 
    Thymol version 2.0.0-beta3 Copyright (C) 2012-2015 James J. Benson
-   <jjbenson AT users.sf.net> (http://www.thymoljs.org/)
+   jjbenson .AT. users.sf.net (http://www.thymoljs.org/)
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
