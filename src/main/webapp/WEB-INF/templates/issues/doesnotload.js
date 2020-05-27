b7799246e110832667719dc57c3df0a9535c7d1e
Jim <jim@chuffy.home>
2020-05-27 23:32:16 +0100

    Prospective fix for #22 - test data files

---
+++ src/main/webapp/WEB-INF/templates/issues/doesnotload.js
@@ -0,0 +1,3 @@
+var div = document.createElement( 'div' );
+div.innerHTML = "This is a message from doesnotload.js.<br/>You could say this is self-contradictory.";
+document.body.append( div );
---
+++ src/main/webapp/WEB-INF/templates/issues/doesnotloadNode.js
@@ -0,0 +1,3 @@
+var div = thymol.thDocument.createElement( 'div' );
+div.innerHTML = "This is a message from doesnotload.js.<br/>You could say this is self-contradictory.";
+thymol.thDocument.body.appendChild( div );
